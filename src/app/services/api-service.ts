c83d2a4c1162c051accda85dacb92df8fceabe36
Wisley Bruno <srwisley.dev@gmail.com>
2024-01-16 11:05:50 -0400

    API V1

--- src/app/services/api-service.ts
+++ src/app/services/api-service.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 
 export function gcsystemInit() {
-    return axios.get("https://gcsystemws.onrender.com/")
+    return axios.get("https://gcsystemws.onrender.com/api/v1/web")
 }
